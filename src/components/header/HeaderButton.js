9431b148edc77ed7aa27e754ae09f493e2b14c36
alireza mohammadkhani <alirezamohammadkhani2@gmail.com>
2022-10-15 13:12:16 +0330

    feat: add button and search box ui

---
+++ src/components/header/HeaderButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from "antd";
+import 'antd/dist/antd.css';
+
+
+
+const HeaderButton = () => {
+  return (
+    <div>
+      <Button type="primary" htmlType="button">اضافه کردن کاربر</Button>
+    </div>
+  );
+};
+
+export default HeaderButton;
--- src/components/header/HeaderSearch.js
+++ src/components/header/HeaderSearch.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import { AudioOutlined } from "@ant-design/icons";
+import { Input, Space } from "antd";
+const { Search } = Input;
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      color: "#1890ff",
+    }}
+  />
+);
+const onSearch = (value) => console.log(value);
 
 const HeaderSearch = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <Space direction="vertical">
+        <Search
+          placeholder="جستجوی کاربر..."
+          onSearch={onSearch}
+          style={{
+            width: 200,
+          }}
+        />
+      </Space>
+    </div>
+  );
 };
 
 export default HeaderSearch;
