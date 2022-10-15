45bf5a5e501afa863449a80fef8d363e42a87478
mahdie <mahdie.ahmadii97@gmail.com>
2022-10-15 13:48:58 +0330

    feat:add form component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { ModalForm } from "./Components/ModalForm"
 
 function App() {
   return (
-    <p>mahdie</p>
+    <>
+
+    </>
   );
 }
 
