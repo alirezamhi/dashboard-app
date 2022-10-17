707241902624de9f361052019f046483eee97e56
unknown <alirezamohammadkhani2@gmail.com>
2022-10-17 20:33:40 +0330

    feat:add loading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useEffect, useState } from 'react';
 import Header from './Components/header/Header';
+import Loading from './Components/Loading/Loading';
 import ShowUser from './Components/ShowUser/ShowUser';
 
 function App() {
+  const [istrue,setIsTrue]=useState(false)
+  useEffect(()=>{
+    setTimeout(() => {
+      setIsTrue(true)
+    }, 3000);
+  },[])
   return (
     <div>
       <Header></Header>
       <hr />
-      <ShowUser></ShowUser>
+      {istrue?<ShowUser/>:<Loading/>}
+      
     </div>
   );
 }
